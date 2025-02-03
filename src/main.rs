4ee696a6cd07cb194112656aee571e860f2842c6
Barizio <adesokanadebare@gmail.com>
2025-02-03 14:47:58 +0100

    added interrupts.rs

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,60 @@
 #![no_std]
 #![no_main]
 
+
 mod writer;
-use bootloader_api::config::Mapping;
 use writer::FrameBufferWriter;
+
+
+use bootloader_api::config::Mapping;
 use x86_64::instructions::hlt;
+// use print_macro::print;
+// use utility_lib::add;
+
+// Use the entry_point macro to register the entry point function:
+//bootloader_api::entry_point!(kernel_main)
+
+
 
 pub static BOOTLOADER_CONFIG: bootloader_api::BootloaderConfig = {
     let mut config = bootloader_api::BootloaderConfig::new_default();
     config.mappings.physical_memory = Some(Mapping::Dynamic);
-    config.kernel_stack_size = 100 * 1024; // 100 KiB
+    config.kernel_stack_size = 100 * 1024; //100KiB
     config
 };
-
 bootloader_api::entry_point!(my_entry_point, config = &BOOTLOADER_CONFIG);
 
-fn my_entry_point(_boot_info: &'static mut bootloader_api::BootInfo) -> ! {
-    let frame_buffer_info = _boot_info.framebuffer.as_mut().unwrap().info();
-    let buffer = _boot_info.framebuffer.as_mut().unwrap().buffer_mut();
+
+mod interrupts;
+use interrupts{init_idt, PICS};
+fn my_entry_point(boot_info: &'static mut bootloader_api::BootInfo) -> ! {
+    let frame_buffer_info = boot_info.framebuffer.as_mut().unwrap().info();
+    let buffer = boot_info.framebuffer.as_mut().unwrap().buffer_mut();
     let mut frame_buffer_writer = FrameBufferWriter::new(buffer, frame_buffer_info);
+   // let mut writer = FrameBufferWriter::new(framebuffer, info);
 
-    frame_buffer_writer.print("Hello, world!\nThis is a test.\n\\cBlue text\\r \tIndented Text");
+    use core::fmt::Write; // Required for writeln!
 
+    // Write multiple lines to test scrolling
+    for i in 0..150 {
+        writeln!(frame_buffer_writer, "This is line number {}", i).unwrap();
+        print!(frame_buffer_writer, "This is printed without\t a \\cnewl\\rine for the bread and egg and the and the and the and the and the and the and the and the and the and the and the and the and the and the and ape {}.\n\n\n", i);
+        // print!(writer, "Hello, World!\n");
+        // print!(writer, "This is a\ttabbed message.\n");
+        // print!(writer, "Here is a custom constant: \\c\n");
+       
+    }
+
+    // Halt the CPU in an infinite loop
     loop {
-        hlt(); // Stop x86_64 from being unnecessarily busy while looping
+        x86_64::instructions::hlt();
     }
 }
 
+
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
-    loop {
-        hlt(); // Halt in case of panic
-    }
+loop{
+hlt();
+}
 }
